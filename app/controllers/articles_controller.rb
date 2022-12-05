b47559ee82450ff540872551fa952f44f9b2b6ac
Clementina Mendes <clementinadesmen@gmail.com>
2022-12-05 15:22:23 +0000

    CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,43 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        if @article.save
+            flash[:notice] = "Article crée avec succés"
+            redirect_to @article   
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to articles_path
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path, status: :see_other
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles#, only: [:show]
+  get "/articles/:id", to: "articles#show"
+  resources :articles, only: [:show,:new,:create,:index, :edit, :update, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
